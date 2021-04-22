a7ad2a5ada6ef0d1ae3618e479335db9feaaf4d0
Witold Jas <jaswitold1@gmail.com>
2021-04-22 15:35:03 +0200

    login test

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Login</Text>
+      <TextInput style={styles.input} />
+      <Text>Pass</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 }
@@ -14,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "100%",
   },
 });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+      <TextInput />
+      <Text>Pass</Text>
+      <TextInput />
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
