3dd2dd442d4449f3de5795944ee4acc8a3b132ec
Witold Jas <jaswitold1@gmail.com>
2021-04-30 13:51:49 +0200

    notifications permissions logic and notifications test on android and iphone

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,19 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Button, Text, SafeAreaView } from "react-native";
 import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 
 const MainScreen = () => {
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS)
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+        return statusObj;
+      })
+      .then((statusObj) => {
+        if (statusObj.granted !== "granted") {
+          return;
+        }
+      });
+  }, []);
   const triggerNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({
       content: {
         title: "Stay Hydrated",
         body: "You should drink Your water now!",
       },
+      trigger: {
+        seconds: 10,
+      },
     });
   };
   return (
     <SafeAreaView style={styles.container}>
       <Text>You have drank</Text>
+      <Button
+        onPress={triggerNotificationHandler}
+        title='I have drank the water'
+      />
     </SafeAreaView>
   );
 };
