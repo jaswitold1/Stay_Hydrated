7aa993f430d0b54c12f6fb65987f96cc4d331b21
Witold Jas <jaswitold1@gmail.com>
2021-05-14 13:08:07 +0200

    implemented button and indicator how much you drank with limit to 8 which goes green

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Button, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Button, Text, SafeAreaView, View } from "react-native";
 import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
 
 const MainScreen = () => {
   const [notificationInterval, setNotificationInterval] = useState({});
+  const [counter, setCounter] = useState(0);
   useEffect(() => {
     Permissions.getAsync(Permissions.NOTIFICATIONS)
       .then((statusObj) => {
@@ -25,28 +26,67 @@ const MainScreen = () => {
       setInterval(() => {
         let date = new Date();
         let hour = date.getHours();
-        if (hour <= 22 && hour >= 10) {
+        if (hour <= 24 && hour >= 10) {
           Notifications.presentNotificationAsync({
             title: "Stay Hydrated",
             body: "You should drink Your water now!",
           });
+        } else {
+          setCounter(0);
         }
-
-        ///// if date = 24 then reset style value of water lvl to 0
       }, 3000)
     );
   };
+  ////2 * 60 * 60 * 1000
   const stopReminding = () => {
     clearInterval(notificationInterval);
     setNotificationInterval({});
   };
 
   const drinkingbuttonHandler = () => {
-    /// setStateOfVariableStyleOfWaterLvl + 1
+    if (counter < 8) {
+      setCounter((prev) => prev + 1);
+    }
   };
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "flex-end",
+      alignItems: "center",
+    },
+    drinkingMeterContainer: {
+      display: "flex",
+      alignItems: "center",
+      width: 200,
+      backgroundColor: counter > 7 ? "lightgreen" : "lightblue",
+      height: counter * 70 + 18,
+    },
+    separator: {
+      marginVertical: 8,
+      borderBottomColor: "black",
+      borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+  });
   return (
     <SafeAreaView style={styles.container}>
-      <Text>You have drank</Text>
+      <View style={styles.drinkingMeterContainer}>
+        {counter < 8 ? (
+          <Text>You have drank {counter} glasses</Text>
+        ) : (
+          <View style={{ display: "flex", alignItems: "center" }}>
+            <Text>Good job ! </Text>
+            <Text>Recommended 8 glasses !</Text>
+          </View>
+        )}
+      </View>
+
+      <Button
+        style={styles.buttonDrink}
+        onPress={() => drinkingbuttonHandler()}
+        title="I'm drinking my water right now!"
+      />
+      <View style={styles.separator} />
+
       <Button onPress={() => startReminding()} title='Set Reminder' />
       <Button onPress={() => stopReminding()} title='Stop reminding' />
     </SafeAreaView>
@@ -54,11 +94,3 @@ const MainScreen = () => {
 };
 
 export default MainScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-});
